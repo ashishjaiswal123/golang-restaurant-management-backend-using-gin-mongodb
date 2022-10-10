22535e0dbf1cc49bbf9b201b182e90a5ccb64f18
ashishjaiswal123 <jaiswalashish025@gmail.com>
2022-10-10 19:37:03 +0530

    UpdateMenu func created in menuController for updating menu

--- models/menuModel.go
+++ models/menuModel.go
@@ -8,10 +8,10 @@ import (
 
 type Menu struct {
 	ID         primitive.ObjectID `bson:"_id"`
-	Name       *string            `json:"name" validate:"required"`
-	Category   *string            `json:"category" validate:"required"`
-	Start_Date time.Time          `json:"start_date"`
-	End_Date   time.Time          `json:"end_date"`
+	Name       string             `json:"name" validate:"required"`
+	Category   string             `json:"category" validate:"required"`
+	Start_Date *time.Time         `json:"start_date"`
+	End_Date   *time.Time         `json:"end_date"`
 	Created_at time.Time          `json:"created_at"`
 	Updated_at time.Time          `json:"updatde_at"`
 	Menu_id    string             `json:"menu_id"`
